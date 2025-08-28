e79e4d75d00dca2c4c5d17af0c28e922cbdea2ae
yuanyu <yuanyi339@gmail.com>
2025-08-28 16:25:41 +0800

    change path of org

--- config.el
+++ config.el
@@ -60,7 +60,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/workstation/org")
 
 ;;设置初始为emacs模式而不是evil
 (setq evil-default-state 'emacs)
