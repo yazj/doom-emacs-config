3cf4a68f4de16a3bc78346cab594e6087ddeafc9
yuanyu <yuanyi339@gmail.com>
2025-09-10 22:57:34 +0800

    add js lsp

--- init.el
+++ init.el
@@ -144,6 +144,7 @@
        ;;janet             ; Fun fact: Janet is me!
        ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       (javascript +lsp)
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        latex             ; writing papers in Emacs has never been so fun
